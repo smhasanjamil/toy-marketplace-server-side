5575da3a229b2826f471b35d0aa0517e4cce48eb
smhasanjamil <smhasanjamil23@gmail.com>
2023-05-20 16:26:12 +0600

    Server connection successfully

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send("Motor Mart is running");
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on port ${port}`);
+});
